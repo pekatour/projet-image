d5cfa1faf0cd32c4e0ea1ca609c2ca2308d3cb42
cyrianR <ragotcyrian@protonmail.com>
2025-03-06 20:36:54 +0100

    refactor meanshift and solve some issues

--- src/tp/meanshift.cpp
+++ src/tp/meanshift.cpp
@@ -10,58 +10,62 @@
 using namespace cv;
 using namespace std;
 
-
-void printHelp(const string& progName)
-{
+/** Print usage for this implementation of meanshift algorithme.
+ */
+void printHelp(const string& progName) {
     cout << "Usage:\n\t " << progName << " <image_file> [<image_ground_truth>]" << endl;
 }
 
-int main(int argc, char** argv)
-{
-    if(argc != 2 && argc != 3)
-    {
+int main(int argc, char** argv) {
+
+    // get passed arguments
+    if(argc != 2 && argc != 3) {
         cout << " Incorrect number of arguments." << endl;
         printHelp(string(argv[0]));
         return EXIT_FAILURE;
     }
-
     const auto imageFilename = string(argv[1]);
-    const string groundTruthFilename = (argc == 3) ? string(argv[2]) : string();;
+    const string groundTruthFilename = (argc == 3) ? string(argv[2]) : string();
 
-    const int kmax = 2;
-    // load the color image to process from file
+    // load image from file
     Mat m;
     m = imread(imageFilename, cv::IMREAD_COLOR);
-    int down_width = 200;
-    int down_height = 200;
- 
-   // resize down
+    
+    // show base image
+    namedWindow("Base image", cv::WINDOW_AUTOSIZE);
+    imshow("Base image", m);
+
+    // resize down image for efficiency
+    const int down_width = 200;
+    const int down_height = 200;
     resize(m, m, Size(down_width, down_height), INTER_LINEAR);
-    namedWindow("Image", cv::WINDOW_AUTOSIZE);
-    imshow("Image", m);
+    namedWindow("Resized image", cv::WINDOW_AUTOSIZE);
+    imshow("Resized image", m);
+
+    // convert image to floats CV_32F
     m.convertTo(m,CV_32F);
-    const int hs = 10;
-    const int hc = 10; // Aucune idée de comment déterminer
-    const float eps = TermCriteria::EPS;
 
-    int k = 10;
-    bool arret = false;
-
+    // meanshift algorithm
+    const int hs = 3; // spatial threshold
+    const int hc = 50; // color threshold
+    const float eps = TermCriteria::EPS; // stop parameter for distance pixel - mean
+    const int kmax = 30; // stop parameter, max iterations
+    int k = 0; // iteration parameter
+    bool arret = false; // true when stop conditions are met
     while (!arret) {
-        k++;
         bool existe = false;
-        for (int i = 0; i < m.rows; i++)
-        {
-            for (int j = 0; j < m.cols; j++)
-            {
-                Vec3f s(0,0,0);
-                float nx = 0;
-                for (int i_xi = max(i-hs,0); i_xi < min(hs + i - 1,m.rows - 1); i_xi++)
-                {   
-                    for (int j_xi = max(i-hs,0); j_xi < min(hs + i, m.cols); j_xi++)
-                    {   
-                        if (cv::norm(m.at<Vec3f>(i_xi,j_xi) - m.at<Vec3f>(i,j)) <= hc)
-                        {
+
+        // iterate over pixels in image
+        for (int i = 0; i < m.rows; i++) {
+            for (int j = 0; j < m.cols; j++) {
+
+                Vec3f s(0,0,0); // sum of neighboring pixels
+                float nx = 0; // total of neighboring pixels
+
+                // iterate over neighboring pixels
+                for (int i_xi = max(i-hs,0); i_xi < min(hs + i - 1,m.rows - 1); i_xi++) {   
+                    for (int j_xi = max(j-hs,0); j_xi < min(hs + j, m.cols); j_xi++) {   
+                        if (cv::norm(m.at<Vec3f>(i_xi,j_xi) - m.at<Vec3f>(i,j)) <= hc) {
                             nx++;
                             s[0] += m.at<Vec3f>(i_xi,j_xi)[0];
                             s[1] += m.at<Vec3f>(i_xi,j_xi)[1];
@@ -70,23 +74,29 @@ int main(int argc, char** argv)
                     }
                 }
                 
+                // compute mean of neighboring pixels
                 Vec3f mh;
                 mh[0] = s[0] / nx;
                 mh[1] = s[1] / nx;
                 mh[2] = s[2] / nx;
 
-                if (cv::norm(mh - m.at<Vec3f>(i,j)) > eps){
-                    existe = true;
-                }
-
+                // update stop condition
+                existe = (cv::norm(mh - m.at<Vec3f>(i,j)) > eps) || existe ;
+                
+                // replace current pixel by mean of neighboring pixels
                 m.at<Vec3f>(i,j) = mh;
             }
         }
 
+        // update parameters
+        k++;
         arret = (k > kmax) && !existe;
     }
-    namedWindow("Image mean-shift", cv::WINDOW_AUTOSIZE);
-    imshow("Image mean-shift", m);
-    waitKey(0);
 
+    // show resulting segmented image using meanshift algorithm
+    namedWindow("Segmented image", cv::WINDOW_AUTOSIZE);
+    imshow("Segmented image", m);
+
+    // wait until esc pressed
+    while(cv::waitKey(1) != 27);
 }
